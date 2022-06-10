13006d375ab0462b990b11b6db8f75fc6739f95b
dankninja08 <dankninja08@gmail.com>
2022-06-10 09:14:15 +0000

    Added thisvid scraper

--- Download Web Video.js
+++ Download Web Video.js
@@ -1,15 +1,27 @@
 // ==UserScript==
 // @name    Download Web Video
-// @version 1
+// @version 2
 // @match   https://soundgasm.net/*
 // @grant   GM_download
 // ==/UserScript==
 
-if (location.hostname === "soundgasm.net") {
-  const filename = `${document.querySelector(".jp-title").innerText}.m4a`;
-  const url = document
-    .querySelector("body")
-    .innerHTML.match(/https:\/\/media.soundgasm.net\/sounds\/.*m4a/)[0];
+let filename;
+let url;
+
+document.onkeydown = (e) => {
+  if (e.key === "/") {
+    if (location.hostname === "soundgasm.net") {
+      filename = `${document.querySelector(".jp-title").innerText}.m4a`;
+      url = document
+        .querySelector("body")
+        .innerHTML.match(/https:\/\/media.soundgasm.net\/sounds\/.*m4a/)[0];
+
+      GM_download(url, filename);
+    } else if (location.hostname === "thisvid.com") {
+      filename = `${document.querySelector("h1").innerText}.mp4`;
+      url = document.querySelector("video").src;
+    }
+  }
 
   GM_download(url, filename);
-}
+};
