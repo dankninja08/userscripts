ec907c18c289e26eb1543a5f96ee56c1ad3881f2
dankninja08 <dankninja08@gmail.com>
2022-06-10 19:57:43 +0000

    Added crazyshit scraper

--- Download Web Video.js
+++ Download Web Video.js
@@ -1,6 +1,7 @@
 // ==UserScript==
 // @name    Download Web Video
-// @version 3
+// @version 4
+// @match   https://crazyshit.com/*
 // @match   https://soundgasm.net/*
 // @match   https://thisvid.com/*
 // @grant   GM_download
@@ -11,14 +12,23 @@ let url;
 
 document.onkeydown = (e) => {
   if (e.key === "/") {
-    if (location.hostname === "soundgasm.net") {
-      filename = `${document.querySelector(".jp-title").innerText}.m4a`;
-      url = document
-        .querySelector("body")
-        .innerHTML.match(/https:\/\/media.soundgasm.net\/sounds\/.*m4a/)[0];
-    } else if (location.hostname === "thisvid.com") {
-      filename = `${document.querySelector("h1").innerText}.mp4`;
-      url = document.querySelector("video").src;
+    switch (location.hostname) {
+      case "soundgasm.net":
+        filename = `${document.querySelector(".jp-title").innerText}.m4a`;
+        url = document
+          .querySelector("body")
+          .innerHTML.match(/https:\/\/media.soundgasm.net\/sounds\/.*m4a/)[0];
+        break;
+
+      case "thisvid.com":
+        filename = `${document.querySelector("h1").innerText}.mp4`;
+        url = document.querySelector("video").src;
+        break;
+
+      case "crazyshit.com":
+        filename = `${document.querySelector(".content_title").innerText}.mp4`;
+        url = document.querySelector("source").src;
+        break;
     }
   }
 
