3f64fed2cc7d698eb6913e4f0490e64231ae2f33
dankninja08 <dankninja08@gmail.com>
2022-06-10 09:45:51 +0000

    Removed duplicate download

--- Download Web Video.js
+++ Download Web Video.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name    Download Web Video
-// @version 2
+// @version 3
 // @match   https://soundgasm.net/*
 // @match   https://thisvid.com/*
 // @grant   GM_download
@@ -16,8 +16,6 @@ document.onkeydown = (e) => {
       url = document
         .querySelector("body")
         .innerHTML.match(/https:\/\/media.soundgasm.net\/sounds\/.*m4a/)[0];
-
-      GM_download(url, filename);
     } else if (location.hostname === "thisvid.com") {
       filename = `${document.querySelector("h1").innerText}.mp4`;
       url = document.querySelector("video").src;
