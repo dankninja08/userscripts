4e7a5aa4ad49fabc628cb28c6836443d86c812f2
dankninja08 <dankninja08@gmail.com>
2022-06-11 19:21:28 +0000

    Added uflash scraper and added progress to tab title bar

--- Download Web Video.js
+++ Download Web Video.js
@@ -1,36 +1,54 @@
 // ==UserScript==
 // @name    Download Web Video
-// @version 5
-// @match   https://crazyshit.com/*
-// @match   https://soundgasm.net/*
-// @match   https://thisvid.com/*
+// @version 6
+// @match   https://crazyshit.com/cnt/medias/*
+// @match   https://soundgasm.net/u/*/*
+// @match   https://thisvid.com/videos/*
+// @match   http://www.uflash.tv/video/*
 // @grant   GM_download
 // ==/UserScript==
 
-let filename;
+let name;
 let url;
 
-document.onkeydown = (e) => {
+document.onkeydown = async (e) => {
   if (e.key === "/") {
     switch (location.hostname) {
+      case "crazyshit.com":
+        name = `${document.querySelector(".content_title").innerText}.mp4`;
+        url = document.querySelector("source").src;
+        break;
+
       case "soundgasm.net":
-        filename = `${document.querySelector(".jp-title").innerText}.m4a`;
+        name = `${document.querySelector(".jp-title").innerText}.m4a`;
         url = document
           .querySelector("body")
           .innerHTML.match(/https:\/\/media.soundgasm.net\/sounds\/.*m4a/)[0];
         break;
 
       case "thisvid.com":
-        filename = `${document.querySelector("h1").innerText}.mp4`;
+        name = `${document.querySelector("h1").innerText}.mp4`;
         url = document.querySelector("video").src;
         break;
 
-      case "crazyshit.com":
-        filename = `${document.querySelector(".content_title").innerText}.mp4`;
+      case "www.uflash.tv":
+        name = `${document.querySelector("h1").innerText}.mp4`;
         url = document.querySelector("source").src;
         break;
     }
 
-    GM_download(url, filename);
+    const title = document.title;
+
+    await GM_download({
+      url,
+      name,
+      onprogress: (progress) => {
+        document.title = `${Math.round(
+          (progress.loaded / progress.total) * 100
+        )}% | ${title}`;
+      },
+    });
+
+    document.title = title;
   }
 };
