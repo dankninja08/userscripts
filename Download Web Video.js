cac25fce8bf240b7109daa14ce6898d8d1c52438
dankninja08 <106836770+dankninja08@users.noreply.github.com>
2022-06-17 18:14:13 -0700

    Switched to redirecting to mp4 source files

--- Download Web Video.js
+++ Download Web Video.js
@@ -1,123 +1,71 @@
 // ==UserScript==
 // @name    Download Web Video
-// @version 8
-// @match   https://crazyshit.com/cnt/medias/*
-// @match   https://www.pornhub.com/view_video.php?viewkey=*
-// @match   https://crazyshit.com/cnt/medias/*
-// @match   https://soundgasm.net/u/*/*
-// @match   https://thisvid.com/videos/*
-// @match   http://www.uflash.tv/video/*
-// @match   https://www.xvideos.com/video*
-// @grant   GM_download
-// @grant   GM_xmlhttpRequest
+// @version 11
+// @match   https://fapster.xxx/videos/*
+// @match   https://www.fpo.xxx/videos/*
+// @match   https://www.mypornhere.com/videos/*
+// @match   https://www.peekvids.com/*
+// @match   https://www.playvids.com/*
+// @match   https://www.pornflip.com/*
+// @match   https://www.pornhits.com/video/*
+// @match   https://www.pornoeggs.com/*
+// @match   https://www.porntrex.com/video/*
+// @match   https://pornwild.com/videos/*
+// @match   https://spankbang.com/*/video/*
+// @match   https://www.tnaflix.com/*
+// @match   https://xhamster.com/videos/*
+// @match   https://www.youcrazyx.com/video/*
+// @match   https://www.yourpornflare.com/video/*
+
+// @exclude https://www.peekvids.com/embed?v=*
+// @exclude https://www.playvids.com/embed/*
+// @exclude https://www.pornflip.com/embed/*
+// @exclude https://www.pornoeggs.com/embed?v=*
 // ==/UserScript==
 
-let name;
-let url;
-let completed = 0;
-
-document.addEventListener("keydown", async (e) => {
-  if (e.key === "/") {
-    switch (location.hostname) {
-      case "crazyshit.com":
-        name = `${document.querySelector(".content_title").innerText}.mp4`;
-        url = document.querySelector("source").src;
-        break;
-
-      case "www.pornhub.com":
-        name = `${document.querySelector("h1").innerText}.ts`;
-        url = eval(document.body.innerHTML.match(/flashvars.*?(?= )/)[0])
-          .mediaDefinitions.find(
-            (def) =>
-              def.quality ===
-              document
-                .querySelector(".mgp_quality li")
-                .innerText.replace("p HD", "")
-          )
-          .videoUrl.replace("master.m3u8", "index-f1-v1-a1.m3u8");
-        break;
-
-      case "soundgasm.net":
-        name = `${document.querySelector(".jp-title").innerText}.m4a`;
-        url = document.body.innerHTML.match(
-          /https:\/\/media.soundgasm.net\/sounds\/.*m4a/
-        )[0];
-        break;
-
-      case "thisvid.com":
-        name = `${document.querySelector("h1").innerText}.mp4`;
-        url = document.querySelector("video").src;
-        break;
-
-      case "www.uflash.tv":
-        name = `${document.querySelector("h1").innerText}.mp4`;
-        url = document.querySelector("source").src;
-        break;
-
-      case "www.xvideos.com":
-        name = `${
-          document
-            .querySelector(".page-title")
-            .innerHTML.match(/.*(?= <span)/)[0]
-        }.ts`;
-        url = document.body.innerHTML
-          .match(/(?<=html5player.setVideoHLS\(').*(?=')/)[0]
-          .replace(
-            ".m3u8",
-            `-${
-              document.querySelector("#levelauto").nextElementSibling.innerText
-            }.m3u8`
-          );
-        break;
-    }
-
-    const title = document.title;
-
-    if (url.includes(".m3u8")) {
-      const res = await get(url);
-
-      const files = res.match(/.*\.ts.*/g);
-      const promises = files.map((filename) =>
-        get(
-          url.replace(url.split("/").pop(), filename),
-          "blob",
-          files.length,
-          title
-        )
-      );
-
-      const blob = new Blob(await Promise.all(promises), {
-        type: "video/mp2t",
-      });
-
-      GM_download(URL.createObjectURL(blob), name);
-    } else {
-      await GM_download({
-        url,
-        name,
-        onprogress: (progress) => {
-          document.title = `${Math.round(
-            (progress.loaded / progress.total) * 100
-          )}% | ${title}`;
-        },
-      });
-    }
-  }
-});
-
-const get = async (url, responseType = "text", total, title) => {
-  return new Promise((resolve, _) => {
-    GM_xmlhttpRequest({
-      url,
-      responseType,
-      onload: (res) => {
-        if (responseType === "blob")
-          document.title = `${Math.round(
-            (++completed / total) * 100
-          )}% | ${title}`;
-
-        resolve(res.response);
-      },
-    });
-  });
+const sites = {
+  "fapster.xxx": () => `/embed/${location.pathname.split("/")[2]}`,
+  "www.fpo.xxx": () => `/embed/${location.pathname.split("/")[2]}`,
+  "www.mypornhere.com": () => `/embed/${location.pathname.split("/")[2]}`,
+  "www.peekvids.com": () =>
+    `/embed?v=${
+      location.search.split("=")[1] || location.pathname.split("/")[3]
+    }`,
+  "www.playvids.com": () =>
+    `/embed/${
+      location.pathname.startsWith("/v/")
+        ? location.pathname.split("/")[2]
+        : location.pathname.split("/")[1]
+    }`,
+  "www.pornflip.com": () =>
+    `/embed/${
+      location.pathname.startsWith("/v/")
+        ? location.pathname.split("/")[2]
+        : location.pathname.split("/")[1]
+    }`,
+  "www.pornhits.com": () => `/embed.php?id=${location.pathname.split("/")[2]}`,
+  "www.pornoeggs.com": () =>
+    `/embed?v=${
+      location.search.split("=")[1] || location.pathname.split("/")[1]
+    }`,
+  "www.porntrex.com": () => `/embed/${location.pathname.split("/")[2]}`,
+  "pornwild.com": () => `/embed/${location.pathname.split("/")[2]}`,
+  "spankbang.com": () => `/embed/${location.pathname.split("/")[1]}`,
+  "www.tnaflix.com": () =>
+    `${location.pathname.split("/").pop().replace("video", "/video/")}`,
+  "xhamster.com": () => `/embed/${location.pathname.split("-").pop()}`,
+  "www.youcrazyx.com": () =>
+    `/embed/${location.pathname.split("-").pop().split(".")[0]}`,
+  "www.yourpornflare.com": () =>
+    `/embed/${location.pathname.split("-").pop().split(".")[0]}`,
 };
+
+const url = new URL(location.href);
+
+url.pathname = sites[location.hostname]().replace("%3F", "?");
+url.hostname = location.hostname.replace(
+  "www.tnaflix.com",
+  "player.tnaflix.com"
+);
+
+window.open(url.href.replace("%3F", "?"));
