9b1685826447c9f796eb6865d3882c54c1710eb1
dankninja08 <106836770+dankninja08@users.noreply.github.com>
2022-06-17 18:20:50 -0700

    Cleaned up chapter support, changed to .url files, and added video preloading

--- Add Chapters to Scene.js
+++ Add Chapters to Scene.js
@@ -1,147 +1,93 @@
 // ==UserScript==
 // @name    Add Chapters to Scene
-// @version 10
-// @match   https://fapster.xxx/embed/*
-// @match   https://www.fpo.xxx/embed/*
-// @match   https://www.mypornhere.com/embed/*
-// @match   https://www.peekvids.com/embed?v=*
-// @match   https://www.playvids.com/embed/*
-// @match   https://www.pornflip.com/embed/*
-// @match   https://www.pornhits.com/embed.php?id=*
-// @match   https://www.pornoeggs.com/embed?v=*
-// @match   https://www.porntrex.com/embed/*
-// @match   https://pornwild.com/embed/*
-// @match   https://spankbang.com/embed/*
-// @match   https://player.tnaflix.com/video/*
-// @match   https://xhamster.com/embed/*
-// @match   https://www.youcrazyx.com/embed/*
-// @match   https://www.yourpornflare.com/embed/*
-
+// @version 11
 // @match   https://*/*.mp4*
+// @match   https://*.fpo.xxx/remote_control.php?file=*
 // @grant   GM_download
+// @grant   GM_xmlhttpRequest
 // ==/UserScript==
 
-if (!location.hash.includes(";")) {
-  const int = setInterval(async () => {
-    const video = document.querySelector("video");
+window.history.replaceState("", "", "/" + location.hash);
 
-    if (video) {
-      if (
-        [
-          "fapster.xxx",
-          "www.fpo.xxx",
-          "www.mypornhere.com",
-          "www.pornhits.com",
-          "www.porntrex.com",
-          "www.pornwild.com",
-          "player.tnaflix.com",
-          "www.youcrazyx.com",
-          "www.yourpornflare.com",
-        ].includes(location.hostname)
-      ) {
-        const url = new URL(video.src);
-        url.hash = `${location.hash};${location.href.split("#")[0]}`;
+document.addEventListener("keydown", (e) => {
+  let times = decodeURIComponent(location.hash.substring(1))
+    .split(";")[0]
+    .split(",")
+    .filter((time) => !!time)
+    .map((time) => parseInt(time));
 
-        location.href = url.href;
-      } else if (
-        [
-          "www.peekvids.com",
-          "www.playvids.com",
-          "www.pornflip.com",
-          "www.pornoeggs.com",
-        ].includes(location.hostname)
-      ) {
-        const res = await fetch(
-          `https://${location.hostname}/v-alt/${video.attributes["data-id"].value}`
-        );
-        const data = await res.json();
+  const video = document.querySelector("video");
+  const currTime = video.currentTime;
 
-        const url = new URL(
-          data["data-src720"] || data["data-src480"] || data["data-src360"]
-        );
-        url.hash = `${location.hash};${location.href.split("#")[0]}`;
+  switch (e.key) {
+    case "ArrowLeft":
+      if (currTime > times[0]) {
+        const i = times.findIndex((time) => time > currTime) - 1;
 
-        location.href = url.href;
-      }
-
-      clearInterval(int);
-    }
-  }, 100);
-} else {
-  window.history.pushState("", "", "/" + location.hash);
-
-  document.addEventListener("keydown", (e) => {
-    let times = decodeURIComponent(location.hash.substring(1))
-      .split(";")[0]
-      .split(",")
-      .filter((time) => !!time)
-      .map((time) => parseInt(time));
-
-    const video = document.querySelector("video");
-    const currTime = video.currentTime;
-
-    switch (e.key) {
-      case "ArrowLeft":
-        if (currTime > times[0]) {
-          const i = times.findIndex((time) => time > currTime) - 1;
-
-          if (i < 0) {
-            if (currTime - times[times.length - 1] < 0.5)
-              video.currentTime = times[times.length - 2];
-            else video.currentTime = times[times.length - 1];
-          } else {
-            if (video.currentTime - times[i] < 0.5)
-              video.currentTime = i > 0 ? times[i - 1] : times[0];
-            else video.currentTime = times[i];
-          }
+        if (i < 0) {
+          if (currTime - times[times.length - 1] < 0.5)
+            video.currentTime = times[times.length - 2];
+          else video.currentTime = times[times.length - 1];
+        } else {
+          if (video.currentTime - times[i] < 0.5)
+            video.currentTime = i > 0 ? times[i - 1] : times[0];
+          else video.currentTime = times[i];
         }
-        break;
+      }
+      break;
 
-      case "ArrowRight":
-        if (currTime < times[times.length - 1])
-          video.currentTime = times[times.findIndex((time) => time > currTime)];
-        break;
+    case "ArrowRight":
+      if (currTime < times[times.length - 1])
+        video.currentTime = times[times.findIndex((time) => time > currTime)];
+      break;
 
-      case ",":
-        video.currentTime = Number.isInteger(currTime)
-          ? currTime - 1
-          : video.paused
-          ? Math.floor(currTime)
-          : Math.floor(currTime - 1);
-        break;
+    case ",":
+      video.currentTime = Number.isInteger(currTime)
+        ? currTime - 1
+        : video.paused
+        ? Math.floor(currTime)
+        : Math.floor(currTime - 1);
+      break;
 
-      case ".":
-        video.currentTime = Number.isInteger(currTime)
-          ? currTime + 1
-          : Math.ceil(currTime);
-        break;
+    case ".":
+      video.currentTime = Number.isInteger(currTime)
+        ? currTime + 1
+        : Math.ceil(currTime);
+      break;
 
-      case "/":
-        if (times.includes(currTime))
-          times = times.filter((t) => t !== currTime);
-        else times = [...times, currTime].sort((a, b) => a - b);
+    case "/":
+      if (times.includes(currTime)) times = times.filter((t) => t !== currTime);
+      else times = [...times, currTime].sort((a, b) => a - b);
 
-        location.hash = `${times.join(",")};${location.hash.split(";")[1]}`;
-        break;
+      location.hash = `${times.join(",")};${location.hash.split(";")[1]}`;
+      break;
 
-      case "s":
-        const url = new URL(location.hash.split(";")[1] || location.href);
-        url.hash = location.hash.split(";")[0];
+    case "d":
+      GM_xmlhttpRequest({
+        url: video.src || document.querySelector("source").src,
+        responseType: "blob",
+        onprogress: (progress) => {
+          document.title = `${Math.round(
+            (progress.loaded / progress.total) * 100
+          )}%`;
+        },
+        onload: (res) => {
+          video.src = URL.createObjectURL(res.response);
+        },
+      });
+      break;
 
-        const file = `<?xml version="1.0" encoding="UTF-8"?>
-      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-      <plist version="1.0">
-      <dict>
-        <key>URL</key>
-        <string>${url.href}</string>
-      </dict>
-      </plist>`;
+    case "s":
+      const url = new URL(location.hash.split(";")[1] || location.href);
+      url.hash = location.hash.split(";")[0];
 
-        GM_download(
-          URL.createObjectURL(new Blob([file])),
-          `${url.hostname.replace("www.", "").split(".")[0]}.webloc`
-        );
-        break;
-    }
-  });
-}
+      const file = `[InternetShortcut]
+URL=${url.href}`;
+
+      GM_download(
+        URL.createObjectURL(new Blob([file])),
+        `${url.hostname.replace("www.", "").split(".")[0]}.url`
+      );
+      break;
+  }
+});
