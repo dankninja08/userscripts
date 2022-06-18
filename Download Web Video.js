99cede08864550541e7266185403d2a69c6d3494
dankninja08 <106836770+dankninja08@users.noreply.github.com>
2022-06-17 18:16:17 -0700

    Switched to redirecting to mp4 source files

--- Download Web Video.js
+++ Download Web Video.js
@@ -1,6 +1,7 @@
 // ==UserScript==
 // @name    Download Web Video
-// @version 11
+// @version 9
+// @match   https://crazyshit.com/cnt/medias/*
 // @match   https://fapster.xxx/videos/*
 // @match   https://www.fpo.xxx/videos/*
 // @match   https://www.mypornhere.com/videos/*
@@ -8,64 +9,127 @@
 // @match   https://www.playvids.com/*
 // @match   https://www.pornflip.com/*
 // @match   https://www.pornhits.com/video/*
+// @match   https://www.pornhits.com/embed.php?id=*
+// @match   https://www.pornhub.com/view_video.php?viewkey=*
 // @match   https://www.pornoeggs.com/*
 // @match   https://www.porntrex.com/video/*
 // @match   https://pornwild.com/videos/*
+// @match   https://soundgasm.net/u/*/*
 // @match   https://spankbang.com/*/video/*
+// @match   https://thisvid.com/videos/*
 // @match   https://www.tnaflix.com/*
+// @match   http://www.uflash.tv/video/*
 // @match   https://xhamster.com/videos/*
+// @match   https://www.xozilla.com/*
+// @match   https://www.xvideos.com/video*
 // @match   https://www.youcrazyx.com/video/*
 // @match   https://www.yourpornflare.com/video/*
-
-// @exclude https://www.peekvids.com/embed?v=*
-// @exclude https://www.playvids.com/embed/*
-// @exclude https://www.pornflip.com/embed/*
-// @exclude https://www.pornoeggs.com/embed?v=*
 // ==/UserScript==
 
-const sites = {
-  "fapster.xxx": () => `/embed/${location.pathname.split("/")[2]}`,
-  "www.fpo.xxx": () => `/embed/${location.pathname.split("/")[2]}`,
-  "www.mypornhere.com": () => `/embed/${location.pathname.split("/")[2]}`,
-  "www.peekvids.com": () =>
-    `/embed?v=${
-      location.search.split("=")[1] || location.pathname.split("/")[3]
-    }`,
-  "www.playvids.com": () =>
-    `/embed/${
-      location.pathname.startsWith("/v/")
-        ? location.pathname.split("/")[2]
-        : location.pathname.split("/")[1]
-    }`,
-  "www.pornflip.com": () =>
-    `/embed/${
-      location.pathname.startsWith("/v/")
-        ? location.pathname.split("/")[2]
-        : location.pathname.split("/")[1]
-    }`,
-  "www.pornhits.com": () => `/embed.php?id=${location.pathname.split("/")[2]}`,
-  "www.pornoeggs.com": () =>
-    `/embed?v=${
-      location.search.split("=")[1] || location.pathname.split("/")[1]
-    }`,
-  "www.porntrex.com": () => `/embed/${location.pathname.split("/")[2]}`,
-  "pornwild.com": () => `/embed/${location.pathname.split("/")[2]}`,
-  "spankbang.com": () => `/embed/${location.pathname.split("/")[1]}`,
-  "www.tnaflix.com": () =>
-    `${location.pathname.split("/").pop().replace("video", "/video/")}`,
-  "xhamster.com": () => `/embed/${location.pathname.split("-").pop()}`,
-  "www.youcrazyx.com": () =>
-    `/embed/${location.pathname.split("-").pop().split(".")[0]}`,
-  "www.yourpornflare.com": () =>
-    `/embed/${location.pathname.split("-").pop().split(".")[0]}`,
+const openUrl = (url) => {
+  const u = new URL(url);
+  u.hash = `${location.hash};${location.href.split("#")[0]}`;
+
+  location.href = u.href;
 };
 
-const url = new URL(location.href);
+document.addEventListener("keydown", async (e) => {
+  if (e.key === "/") {
+    let name;
+    let url;
 
-url.pathname = sites[location.hostname]().replace("%3F", "?");
-url.hostname = location.hostname.replace(
-  "www.tnaflix.com",
-  "player.tnaflix.com"
-);
+    let quality;
+    let res;
+    let data;
 
-window.open(url.href.replace("%3F", "?"));
+    const video = document.querySelector("video");
+    const src = video?.src || video?.children[0]?.src;
+
+    switch (location.hostname) {
+      case "www.pornhits.com":
+        if (location.pathname.startsWith("/video/")) {
+          openUrl(
+            `http://www.pornhits.com/embed.php?id=${
+              location.pathname.split("/")[2]
+            }`
+          );
+          return;
+        }
+        break;
+
+      case "www.peekvids.com":
+      case "www.playvids.com":
+      case "www.pornflip.com":
+      case "www.pornoeggs.com":
+        quality = document.querySelector(
+          ".mediaPlayerSelect .labelText"
+        ).innerText;
+
+        res = await fetch(
+          `https://${location.hostname}/v-alt/${video.attributes["data-id"].value}`
+        );
+        data = await res.json();
+
+        url = data[`data-src${quality}`];
+        break;
+
+      case "www.pornhub.com":
+        quality = parseInt(
+          document.querySelector(".mgp_quality .mgp_value").innerText
+        );
+
+        name = `${document.querySelector("h1").innerText}.mp4`;
+        res = await fetch(
+          eval(
+            document.body.innerHTML.match(/flashvars.*?(?= )/)[0]
+          ).mediaDefinitions.find((def) => def.format === "mp4").videoUrl
+        );
+        data = await res.json();
+
+        url = data.find((def) => parseInt(def.quality) === quality).videoUrl;
+        location.href = url;
+        break;
+
+      case "crazyshit.com":
+      case "thisvid.com":
+      case "www.uflash.tv":
+        name = `${document.querySelector("h1").innerText}.mp4`;
+        break;
+
+      case "soundgasm.net":
+        name = `${document.querySelector(".jp-title").innerText}.m4a`;
+        url = document.body.innerHTML.match(
+          /https:\/\/media.soundgasm.net\/sounds\/.*m4a/
+        )[0];
+        break;
+
+      case "www.xvideos.com":
+        quality = document.querySelector("#quality").innerText.split(" ")[1];
+
+        res = await fetch(
+          document.body.innerHTML.match(
+            /(?<=html5player.setVideoHLS\(').*(?=')/
+          )[0]
+        );
+        data = await res.text();
+
+        await navigator.clipboard.writeText(
+          `ffmpeg -i "${url.replace(
+            /hls.m3u8.*/,
+            data.match(new RegExp(`hls-${quality}.*`))[0]
+          )}" "./Downloads/${`${
+            document
+              .querySelector(".page-title")
+              .innerText.split(/ [0-9]+ [sec|min]/)[0]
+          }.mp4`}"`
+        );
+        return;
+    }
+
+    if (name) await navigator.clipboard.writeText(name);
+
+    if (url) openUrl(url);
+    else if (src) openUrl(src);
+    else alert("Start playing the video!");
+  }
+});
