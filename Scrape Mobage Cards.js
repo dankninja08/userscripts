3a6955112cf091485b2f3b50e2922c76ee8ffcc2
dankninja08 <106836770+dankninja08@users.noreply.github.com>
2022-06-30 18:48:32 -0700

    Switched to XHR for compatibility

--- Scrape Mobage Cards.js
+++ Scrape Mobage Cards.js
@@ -1,26 +1,59 @@
 // ==UserScript==
 // @name    Scrape Mobage Cards
-// @version 3
+// @version 4
 // @match   https://sp.mbga.tv/hsdd/list*
-// @grant   GM_download
+// @grant   GM_xmlhttpRequest
 // ==/UserScript==
 
-document.addEventListener("keydown", (e) => {
-  if (e.key === "/") {
-    [...document.querySelectorAll('a[href^="/hsdd/card/"]')].forEach((card) => {
-      const title = card.innerText.split("(")[0];
-
-      if (!title.includes("EX")) {
-        [...card.querySelectorAll("img")].forEach((img, i) => {
-          GM_download({
-            url: img.src.replace("/r/", "/l/"),
-            name: `${title} - ${i + 1}.jpeg`,
-            headers: {
-              referer: "https://sp.mbga.tv/",
-            },
-          });
-        });
-      }
+const get = async (url) =>
+  new Promise((resolve, _) => {
+    GM_xmlhttpRequest({
+      url,
+      responseType: 'blob',
+      headers: {
+        referer: 'https://sp.mbga.tv/',
+      },
+      onload: (data) => {
+        resolve(data.response);
+      },
     });
+  });
+
+const pause = async () =>
+  new Promise((resolve, _) => {
+    setTimeout(() => {
+      resolve();
+    }, 1000);
+  });
+
+document.addEventListener('keydown', async (e) => {
+  if (e.key === '/') {
+    const links = [];
+
+    for (const card of [
+      ...document.querySelectorAll('a[href^="/hsdd/card/"]'),
+    ]) {
+      const title = card.innerText.split('(')[0];
+
+      if (!title.includes('EX')) {
+        for (const [i, img] of [...card.querySelectorAll('img')].entries()) {
+          const data = await get(img.src.replace('/r/', '/l/'));
+
+          const link = document.createElement('a');
+          link.href = URL.createObjectURL(data);
+          link.download = `${title} - ${i + 1}.jpeg`;
+
+          links.push(link);
+        }
+      }
+    }
+
+    while (links.length > 0) {
+      for (const link of links.splice(0, 10)) {
+        link.click();
+      }
+
+      await pause();
+    }
   }
 });
